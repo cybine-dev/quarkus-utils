0a1970ef7795e3ea51cf3d3ac9daac69a80c5c1f
Fabio Boelter <boelter.fabio@cybine.de>
2024-03-14 13:33:00 +0100

    document configuration options

--- api-query/src/main/java/de/cybine/quarkus/config/ApiQueryConfig.java
+++ api-query/src/main/java/de/cybine/quarkus/config/ApiQueryConfig.java
@@ -3,19 +3,21 @@ package de.cybine.quarkus.config;
 import io.quarkus.runtime.annotations.*;
 import io.smallrye.config.*;
 
+import static de.cybine.quarkus.util.FilePathHelper.*;
+
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
 @ConfigMapping(prefix = "quarkus.cybine.api.query")
-@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public interface ApiQueryConfig
 {
     /**
-     *
+     * Defines if it is allowed to write api-queries that result in nested joins in the sql output
      */
     @WithDefault("false")
     @WithName("allow-multi-level-relations")
     boolean allowMultiLevelRelations( );
 
     /**
-     *
+     * Defines paths to additional configuration files
      */
     @WithName("paths")
     FilePaths paths( );
@@ -23,17 +25,17 @@ public interface ApiQueryConfig
     interface FilePaths
     {
         /**
-         *
+         * Defines the path to the RBAC (role-based access control) configuration file
          */
         @WithName("rbac-path")
-        @WithDefault("%resources%/rbac.json")
+        @WithDefault(RESOURCES_PLACEHOLDER + "/rbac.json")
         String rbacPath( );
 
         /**
-         *
+         * Defines the path to the api-permission file
          */
         @WithName("api-permissions-path")
-        @WithDefault("%resources%/api-permissions.json")
+        @WithDefault(RESOURCES_PLACEHOLDER + "/api-permissions.json")
         String apiPermissionsPath( );
     }
 }
--- type-converter/src/main/java/de/cybine/quarkus/config/TypeConverterConfig.java
+++ type-converter/src/main/java/de/cybine/quarkus/config/TypeConverterConfig.java
@@ -9,28 +9,38 @@ import io.smallrye.config.*;
 public interface TypeConverterConfig
 {
     /**
-     *
+     * Defines default maximum of nested objects to consider while executing converters. Additional layers of nested
+     * objects will be ignored.
      */
     @WithDefault("5")
     @WithName("default.max-depth")
     int maxDepth( );
 
     /**
-     *
+     * Defines if null values will be inserted into collections.
      */
     @WithDefault("true")
     @WithName("default.filter-null-values")
     boolean filterNullValues( );
 
     /**
-     *
+     * Defines if converters may produce empty collections. If false, empty collections will be replaced with null.
      */
     @WithDefault("false")
     @WithName("default.allow-empty-collections")
     boolean allowEmptyCollections( );
 
     /**
+     * Default policy to define handling of duplicate objects. If an object has been identified to be a duplicate of
+     * another object further down in the conversion tree, the defined duplicate-policy will be used to handle a
+     * possible loop.
      *
+     * <ul>
+     *     <li><b>PROCESS</b> ignores the output of the duplicate check and always executes the converter</li>
+     *     <li><b>IGNORE_ITEM</b> skips a single item. While processing collections the item will be replaced with
+     *     null.</li>
+     *     <li><b>IGNORE_ALL</b> skips all items. While processing collections there will be no converted items.</li>
+     * </ul>
      */
     @WithDefault("IGNORE_ALL")
     @WithName("default.duplicate-policy")
