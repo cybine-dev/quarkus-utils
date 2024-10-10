8457562da322cb0c90ee8ddbcb238f3f920e2c2a
Fabio Boelter <boelter.fabio@cybine.de>
2024-10-10 11:53:07 +0200

    add api-client extension

--- api-client-deployment/build.gradle.kts
+++ api-client-deployment/build.gradle.kts
@@ -9,6 +9,9 @@ dependencies {
     implementation(project(":type-converter-deployment"))
     implementation("io.quarkus:quarkus-arc-deployment")
     implementation("io.quarkus:quarkus-core-deployment")
+    implementation("io.quarkus:quarkus-hibernate-validator-deployment")
+    implementation("io.quarkus:quarkus-jackson-deployment")
+    implementation("io.quarkus:quarkus-smallrye-openapi-deployment")
 
     testImplementation(project(":test-utils"))
     testImplementation("io.quarkus:quarkus-junit5")
